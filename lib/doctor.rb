bb465ffd3a679ffcc438415ced7a09a4c624ca3f
Owen Mowery <owenmowery99@gmail.com>
2021-03-10 15:33:37 -0500

    commit tree 873867ee3b89d45648deb31f7f95d88861e8a7bb

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,33 @@
+class Doctor
 
+    attr_accessor :name
+    @@all = []
+  
+    def initialize(name)
+      @name = name
+      @@all << self
+    end
+  
+    def appointments
+      Appointment.all.select {|appointment| appointment.doctor == self}
+    end
+  
+    def patients
+      doctor_patients = []
+      Appointment.all.each do |appointment| 
+      if appointment.doctor == self
+        doctor_patients << appointment.patient
+      end
+    end
+    doctor_patients
+    end
+  
+    def self.all
+      @@all
+    end
+  
+    def new_appointment(date, patient)
+      Appointment.new(date, patient, self)
+    end
+  
+end
--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,26 @@
+class Genre
+    
+    attr_reader :name
+    @@all = []
 
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all.select {|song| song.genre == self}
+    end
+
+    def artists
+        Song.all.collect do |song| 
+            if song.genre == self
+                song.artist
+            end
+        end
+    end
+end
